84bc8e5e1005cdbc43b74506a56df422cec88d4d
David Howlett <david.howlett3@gmail.com>
2018-07-21 20:36:37 +0100

    removed a loop from C code

--- C/7.c
+++ C/7.c
@@ -1,21 +1,28 @@
+/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+we can see that the 6th prime is 13.
+
+What is the 10001st prime number?
+*/
+
 #include <math.h>
 #include <stdio.h>
 const int maxPrime = 200000;
-int primeToFind=10001;
+int primesToFind=10001;
 char x[maxPrime];
-int i=0;
-int main()
-{
-	for(int i=0;i<maxPrime;i++)
+int i;
+int main() {
+    // This is an implementation of the Sieve of Eratosthenes
+    // I initially set all the numbers to be possibly prime
+	for(i=2;i<maxPrime;i++)
 		x[i]=1;
-	x[0]=x[1]=0;// this manually sets 0 and 1 to be not prime
-	for(int i=2;i<1+sqrt(maxPrime);i++)
-		if(x[i])
+
+	for(i=2;primesToFind;){
+		if(x[++i]){
 			for(int j=2*i;j<maxPrime;j+=i)
 				x[j]=0;
-	int primeCount=0;
-	for(i=0;i<maxPrime&&primeCount<primeToFind;)
-		if(x[++i])
-			primeCount++;
+            primesToFind--;
+        }
+    }
 	printf("%d\n",i);	// should be: 104743
 }
