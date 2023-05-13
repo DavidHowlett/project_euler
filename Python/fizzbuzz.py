aac453ef2bd749779ad57f421fb5d731ce85990a
David Howlett <david.howlett3@gmail.com>
2023-05-13 14:28:16 +0100

    pass pre-commit

--- Python/fizzbuzz.py
+++ Python/fizzbuzz.py
@@ -30,9 +30,9 @@ for i in range(1, 100):
     print((i if i % 3 else 'fizz') if i % 5 else ('buzz' if i % 3 else 'fizzbuzz'))
 
 for i in range(1, 100):
-    print([['fizzbuzz', 'fizz'], ['buzz', i]][bool(i % 3)][bool(i % 5)])
+    print([['fizzbuzz', 'fizz'], ['buzz', i]][bool(i % 3)][bool(i % 5)])  # type: ignore
 
 for i in range(1, 100):
-    print([[i, 'buzz'], ['fizz', 'fizzbuzz']][i % 3 == 0][i % 5 == 0])
+    print([[i, 'buzz'], ['fizz', 'fizzbuzz']][i % 3 == 0][i % 5 == 0])  # type: ignore
 
-[print([[i, 'buzz'], ['fizz', 'fizzbuzz']][i % 3 == 0][i % 5 == 0]) for i in range(1, 100)]
+[print([[i, 'buzz'], ['fizz', 'fizzbuzz']][i % 3 == 0][i % 5 == 0]) for i in range(1, 100)]  # type: ignore
